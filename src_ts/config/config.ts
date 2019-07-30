fad3b0fbfc0356fbee1d3b26a58c835d569016fa
Adrian Hangan <iovuadrian@gmail.com>
2019-07-30 16:31:27 +0300

    routing updates

--- src_ts/config/config.ts
+++ src_ts/config/config.ts
@@ -4,7 +4,7 @@ const DEV_DOMAIN = 'etools-dev.unicef.org';
 const DEMO_DOMAIN = 'etools-demo.unicef.org';
 
 export const SMALL_MENU_ACTIVE_LOCALSTORAGE_KEY: string = 'etoolsAppSmallMenuIsActive';
-export const ROOT_PATH: string = '/';
+export const ROOT_PATH: string = '/psea/';
 
 export const isProductionServer = () => {
   const location = window.location.href;
