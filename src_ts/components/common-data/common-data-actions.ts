1f7cfe6f75789eea211cf7dd0320f9db238518bb
Adrian Hangan <iovuadrian@gmail.com>
2019-08-09 11:02:52 +0300

    added redux commonData

---
+++ src_ts/components/common-data/common-data-actions.ts
@@ -0,0 +1,10 @@
+import {EtoolsCommonData} from './common-data';
+import './common-data';
+
+const commonDataEl = document.createElement('etools-common-data') as EtoolsCommonData;
+
+export const getUnicefUsersData = () => {
+  return commonDataEl.getUnicefUserData();
+};
+
+
