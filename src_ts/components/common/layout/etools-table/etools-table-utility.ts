261fc607b109dcadc5efc643ea2b0a8d5db61403
Adrian Hangan <iovuadrian@gmail.com>
2019-08-13 14:37:05 +0300

    engagements list: paginated dummy data based on filters, sort and paginator, url updates

--- src_ts/components/common/layout/etools-table/etools-table-utility.ts
+++ src_ts/components/common/layout/etools-table/etools-table-utility.ts
@@ -3,9 +3,11 @@
  */
 
 import {EtoolsTableColumn, EtoolsTableColumnSort} from './etools-table';
+import {GenericObject} from '../../../../types/globals';
 
 export interface EtoolsTableSortItem {
-  [key: string]: EtoolsTableColumnSort;
+  name: string;
+  sort: EtoolsTableColumnSort;
 }
 
 export const getUrlQueryStringSort = (sortFields: EtoolsTableSortItem[]): string => {
@@ -26,12 +28,61 @@ export const getSortFields = (columns: EtoolsTableColumn[]): EtoolsTableSortItem
   return sortItems;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getSortFieldsFromUrlParams = (params: any) => {
-  console.log(params);
-  // TODO: used to init sort object from url params (API related)
+export const getSortFieldsFromUrlSortParams = (param: string): EtoolsTableSortItem[] => {
+  const sortFields: EtoolsTableSortItem[] = param.split('|').map((sort: string) => {
+    const s = sort.split('.');
+    // eslint-disable-next-line @typescript-eslint/no-object-literal-type-assertion
+    const sortItem = {
+      name: s[0],
+      sort: s[1]
+    } as EtoolsTableSortItem;
+    return sortItem;
+  });
+  return sortFields;
 };
 
-export const getQueryStringFromObject = () => {
-  // TODO
+export const buildUrlQueryString = (params: GenericObject): string => {
+  const queryParams = [];
+
+  for (const param in params) {
+    if (params[param]) {
+      const paramValue = params[param];
+      let filterUrlValue;
+
+      if (paramValue instanceof Array && paramValue.length > 0) {
+        filterUrlValue = paramValue.join('|');
+      } else if (typeof paramValue === 'boolean') {
+        if (paramValue) { // ignore if it's false
+          filterUrlValue = 'true';
+        }
+      } else {
+        if (!(param === 'page' && paramValue === 1)) { // do not include page if page=1
+          filterUrlValue = String(paramValue).trim();
+        }
+      }
+
+      if (filterUrlValue) {
+        queryParams.push(param + '=' + filterUrlValue);
+      }
+    }
+  }
+
+  return queryParams.join('&');
+};
+
+export const getSelectedFiltersFromUrlParams = (selectedFilters: GenericObject,
+                                                params: GenericObject): GenericObject => {
+  const filters: GenericObject = {...selectedFilters};
+  for (const param in params) {
+    if (params[param]) {
+      if (filters[param] instanceof Array) {
+        filters[param] = params[param].split('|');
+      } else if (typeof filters[param] === 'boolean') {
+        filters[param] = params[param] === 'true';
+      } else {
+        filters[param] = params[param];
+      }
+    }
+  }
+  return filters;
 };
