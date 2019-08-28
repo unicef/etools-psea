eabf902424bdc931453183bf1f9864e4f34eb312
danNordlogic <dan.cioara@nordlogic.com>
2019-08-28 16:39:37 +0300

    Assessing firm - Enter po number

--- src_ts/components/pages/engagements/engagement-tab-pages/details/assessing-firm.ts
+++ src_ts/components/pages/engagements/engagement-tab-pages/details/assessing-firm.ts
@@ -8,7 +8,6 @@ import {PaperInputElement} from '@polymer/paper-input/paper-input';
 import {SharedStylesLit} from '../../../../styles/shared-styles-lit';
 import {getEndpoint} from '../../../../../endpoints/endpoints';
 import {makeRequest} from '../../../../utils/request-helper';
-import {cloneDeep} from 'lodash-es';
 
 /**
  * @customElement
@@ -28,7 +27,7 @@ class AssessingFirm extends LitElement {
       <div class="row-padding-v">
         <paper-input id="poNumber" label="Enter PO Number" always-float-label
           class="input-width"
-          .value="${this.engagement.order_number}"
+          .value="${this.assessor.order_number}"
           @value-changed=${(e: CustomEvent) => this._updateEngagementPoNumber((e.target! as PaperInputElement).value!)}
           allowed-pattern="[0-9]"
           max-length=10
@@ -37,10 +36,10 @@ class AssessingFirm extends LitElement {
         </paper-input>
       </div>
       <div class="layout-vertical row-padding-v"
-          ?hidden="${this._hideFirmName(this.engagement.name, this.requestInProgress)}">
+          ?hidden="${this._hideFirmName(this.assessor.auditor_firm_name, this.requestInProgress)}">
         <label class="paper-label">Firm Name</label>
         <label class="input-label row-padding-v">
-          ${this.engagement.name}
+          ${this.assessor.auditor_firm_name}
           <paper-spinner ?hidden="${!this.requestInProgress}" ?active="${this.requestInProgress}"></paper-spinner>
         </label>
 
@@ -51,15 +50,14 @@ class AssessingFirm extends LitElement {
   @property({type: String})
   prevOrderNumber: string = '';
 
-  @property({type: Object})
-  engagement: GenericObject = {name: ''};
+  @property({type: String})
+  currentOrderNumber: string = '';
 
   @property({type: Object})
-  assessor!: GenericObject;
+  assessor: GenericObject = {auditor_firm_name: ''};
 
   @property({type: Boolean})
   requestInProgress: boolean = false;
-
 
   _getFirmName() {
 
@@ -67,15 +65,14 @@ class AssessingFirm extends LitElement {
       return;
     }
 
-    if (Number(this.engagement.order_number) === Number(this.prevOrderNumber)) {
+    if (Number(this.currentOrderNumber) === Number(this.prevOrderNumber)) {
       return;
     }
     this.requestInProgress = true;
 
-    makeRequest(getEndpoint('auditorFirm', {id: this.engagement.order_number}))
+    makeRequest(getEndpoint('auditorFirm', {id: this.currentOrderNumber}))
       .then((resp: any) => {
-        this.assessor = {auditor_firm: resp.auditor_firm.id, order_number:  resp.order_number};
-        this.engagement = {name: resp.auditor_firm.name, order_number: resp.order_number};
+        this.assessor = {auditor_firm: resp.auditor_firm.id, order_number: resp.order_number, auditor_firm_name: resp.auditor_firm.name};
         this.prevOrderNumber = resp.order_number;
         this.requestInProgress = false;
         this.dispatchEvent(new CustomEvent('firm-changed', {
@@ -88,7 +85,7 @@ class AssessingFirm extends LitElement {
   }
 
   _validatePONumber() {
-    const poNumber = this.engagement.order_number;
+    const poNumber = this.currentOrderNumber;
     const valid = poNumber && poNumber.length === 10;
     const poNumberElem = this.shadowRoot!.querySelector('#poNumber') as PaperInputElement;
     if (poNumberElem) {
@@ -97,8 +94,9 @@ class AssessingFirm extends LitElement {
 
     return valid;
   }
+
   _updateEngagementPoNumber(newVal: string) {
-    this.engagement.order_number = newVal;
+    this.currentOrderNumber = newVal;
   }
 
   _hideFirmName(firmName: string, requestInProgress: boolean) {
