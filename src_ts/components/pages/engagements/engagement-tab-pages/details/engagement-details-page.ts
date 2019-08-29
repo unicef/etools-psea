ffe0240e29d6638a738e62d4006038000b94fe74
Adriana Trif <trif.corina.adriana@gmail.com>
2019-08-29 21:15:08 +0300

    Create assessor

--- src_ts/components/pages/engagements/engagement-tab-pages/details/engagement-details-page.ts
+++ src_ts/components/pages/engagements/engagement-tab-pages/details/engagement-details-page.ts
@@ -3,7 +3,6 @@ import './assessment-info';
 import './assessor-info';
 import './firm-staff-members';
 import {LitElement, html, property, customElement} from 'lit-element';
-import {FirmStaffMembersEl} from './firm-staff-members';
 import {store, RootState} from '../../../../../redux/store';
 import {connect} from 'pwa-helpers/connect-mixin';
 import {SharedStylesLit} from '../../../../styles/shared-styles-lit';
@@ -28,7 +27,6 @@ class EngagementDetailsPage extends connect(store)(LitElement) {
 
       <assessment-info></assessment-info>
       <assessor-info ?hidden="${this.isNew}" .assessmentId="${this.assessmentId}" @firm-changed="${this.firmChanged}"></assessor-info>
-      <firm-staff-members hidden id="firmStaffMembers"></firm-staff-members>
     `;
   }
 
@@ -48,11 +46,7 @@ class EngagementDetailsPage extends connect(store)(LitElement) {
     }
   }
 
-  firmChanged(e: CustomEvent) {
-    let firmStaffMembersEl = this.shadowRoot!.querySelector('#firmStaffMembers') as FirmStaffMembersEl;
-    firmStaffMembersEl.hidden = false;
-    firmStaffMembersEl.populateStaffMembersList(e.detail.id);
-  }
+
 
 
 }
