f194ab1f5d3c7d8693c897c5f773717afa0219c6
danNordlogic <49279861+danNordlogic@users.noreply.github.com>
2019-09-10 22:19:02 +0300

    [ch14543] External Assessor - validate external individual (#47)
    * [ch14543] External Assessor - make sure toast msg are displayed in case the new entered external individual exists/has unicef.org email.
    
    * [ch14543] External Assessor - validate external individual

--- src_ts/components/pages/assessments/assessment-tab-pages/details/staff-member-dialog.ts
+++ src_ts/components/pages/assessments/assessment-tab-pages/details/staff-member-dialog.ts
@@ -36,10 +36,10 @@ export class StaffMemberDialog extends LitElement {
                       ?opened="${this.dialogOpened}"
                       dialog-title="${this.dialogTitle}"
                       size="md"
-                      ?show-spinner="${this.requestInProcess}"
+                      ?show-spinner="${this.requestInProgress}"
                       @close="${this.handleDialogClosed}"
                       ok-btn-text="${this.confirmBtnText}"
-                      ?disable-confirm-btn="${this.requestInProcess}"
+                      ?disable-confirm-btn="${this.requestInProgress}"
                       keep-dialog-open
                       @confirm-btn-clicked="${this.onSaveClick}">
 
@@ -150,7 +150,7 @@ export class StaffMemberDialog extends LitElement {
   dialogOpened: boolean = false;
 
   @property({type: Boolean, reflect: true})
-  requestInProcess: boolean = false;
+  requestInProgress: boolean = false;
 
   @property({type: String})
   dialogTitle!: string;
@@ -232,7 +232,7 @@ export class StaffMemberDialog extends LitElement {
 
   private saveDialogData() {
     this.getControlsData();
-    this.requestInProcess = true;
+    this.requestInProgress = true;
 
     const options = {
       method: this.isNewRecord ? 'POST' : 'PATCH',
@@ -241,20 +241,19 @@ export class StaffMemberDialog extends LitElement {
 
     makeRequest(options, this.editedItem)
       .then((resp: any) => this._handleResponse(resp))
-      .catch((err: any) => this._handleError(err));
+      .catch((err: any) => this._handleError(err))
+      .then(() => this.requestInProgress = false);
   }
 
   _handleResponse(resp: any) {
-    this.requestInProcess = false;
     fireEvent(this, 'staff-member-updated', {...resp, hasAccess: this.editedItem.hasAccess});
     this.handleDialogClosed();
   }
 
   _handleError(err: any) {
-    this.requestInProcess = false;
-    const msg = 'Failed to save/update new Firm Staff Member!';
+    let msg = formatServerErrorAsText(err);
     logError(msg, 'staff-member', err);
-    fireEvent(this.toastEventSource, 'toast', {text: formatServerErrorAsText(err)});
+    fireEvent(this.toastEventSource, 'toast', {text: msg});
   }
 
   getEl(elName: string): HTMLInputElement {
@@ -262,4 +261,3 @@ export class StaffMemberDialog extends LitElement {
   }
 
 }
-
