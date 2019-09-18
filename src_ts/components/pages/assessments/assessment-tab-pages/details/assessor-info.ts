fb28617ad3ff3c01b73d2f6b8bbf76dda8ee5d59
Adriana Trif <trif.corina.adriana@gmail.com>
2019-09-18 15:59:15 +0300

    Fix handle err on save assessor

--- src_ts/components/pages/assessments/assessment-tab-pages/details/assessor-info.ts
+++ src_ts/components/pages/assessments/assessment-tab-pages/details/assessor-info.ts
@@ -260,12 +260,13 @@ export class AssessorInfo extends connect(store)(LitElement) {
     }
 
     store.dispatch(saveAssessorData(this.assessment.id as number,
-      this.assessor.id, this.collectAssessorData(), this.handleAssessorSaveError.bind(this))).then(() => {
-      // update assessor in assessment object
-      const assessorName = this.getAssessorName();
-      if (assessorName) {
-        store.dispatch(updateAssessmentData({...this.assessment, assessor: assessorName}));
-      }
+      this.assessor.id, this.collectAssessorData(), this.handleAssessorSaveError.bind(this)))
+        .then(() => {
+        // update assessor in assessment object
+        const assessorName = this.getAssessorName();
+        if (assessorName) {
+          store.dispatch(updateAssessmentData({...this.assessment, assessor: assessorName}));
+        }
     });
   }
 
@@ -287,6 +288,7 @@ export class AssessorInfo extends connect(store)(LitElement) {
   handleAssessorSaveError(error: any) {
     logError(error);
     fireEvent(this, 'toast', {text: formatServerErrorAsText(error), showCloseBtn: true});
+    throw new Error(error.message);
   }
 
   collectAssessorData() {
