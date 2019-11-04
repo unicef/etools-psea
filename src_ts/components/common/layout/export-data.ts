416ba05d0b6277dc7385355c0e2e798b51823126
Andrei Lazar <lazaryandrei@gmail.com>
2019-11-04 17:28:29 +0200

    style adjustments

--- src_ts/components/common/layout/export-data.ts
+++ src_ts/components/common/layout/export-data.ts
@@ -27,7 +27,9 @@ export class ExportData extends LitElement {
         }
         
         paper-button:focus {
-          @apply --paper-material-elevation-2
+          box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+          0 1px 10px 0 rgba(0, 0, 0, 0.12),
+          0 2px 4px -1px rgba(0, 0, 0, 0.4);
         }
         
         paper-button iron-icon {
