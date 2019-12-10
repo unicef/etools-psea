8638a5dacd57f7c788196a2d46006c3f46e26707
dcioara <dan.cioara@nordlogic.com>
2019-12-10 14:12:54 +0200

    [ch17296] Responsive app header

--- src_ts/components/app-shell/header/countries-dropdown-styles.ts
+++ src_ts/components/app-shell/header/countries-dropdown-styles.ts
@@ -57,6 +57,12 @@ export const countriesDropdownStyles = html`
         max-height: 380px;
       };
     }
+
+    @media (max-width: 768px) {
+      etools-dropdown {
+        width: 130px;
+      }
+    }
   </style>
 
 `;
