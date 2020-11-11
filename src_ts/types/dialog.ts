8ff1b03509ca6ad09847d2e0b71c4c02aaa09ac3
Dan <49279861+danNordlogic@users.noreply.github.com>
2020-11-11 12:09:05 +0200

    ch23620] Show confirmation dialog on delete saved attachment (#287)

---
+++ src_ts/types/dialog.ts
@@ -0,0 +1,11 @@
+export interface IDialog<D> {
+  dialog: string;
+  dialogData?: D;
+  readonly?: boolean;
+}
+export interface IEtoolsDialogResponse {
+  confirmed: boolean;
+}
+export interface IDialogResponse<R> extends IEtoolsDialogResponse {
+  response?: R;
+}
