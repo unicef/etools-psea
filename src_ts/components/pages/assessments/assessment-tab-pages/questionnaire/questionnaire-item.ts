aaed6a03976fd0c82edd46aa8740da1b025ca6d5
Adriana Trif <trif.corina.adriana@gmail.com>
2021-01-18 18:29:12 +0200

    Remove page refresh after save attachments

--- src_ts/components/pages/assessments/assessment-tab-pages/questionnaire/questionnaire-item.ts
+++ src_ts/components/pages/assessments/assessment-tab-pages/questionnaire/questionnaire-item.ts
@@ -191,9 +191,7 @@ export class QuestionnaireItemElement extends LitElement {
       .catch((err: any) => {
         fireEvent(this, 'toast', {text: formatServerErrorAsText(err)});
       })
-      .then(() => (this.showLoading = false))
-      // the code bellow is a workaround for a slow db response issue on the production env
-      .finally(() => location.reload());
+      .then(() => (this.showLoading = false));
   }
 
   /**
