e0f4768758e160276326187ec68e986dd79cfaaa
dcioara <dan.cioara@nordlogic.com>
2021-05-18 11:38:03 +0300

    fix

--- src_ts/components/pages/assessments/assessment-tab-pages/details/assessment-info.ts
+++ src_ts/components/pages/assessments/assessment-tab-pages/details/assessment-info.ts
@@ -113,7 +113,9 @@ export class AssessmentInfo extends connect(store)(PermissionsMixin(LitElement))
               option-label="label"
               trigger-value-change-event
               @etools-selected-item-changed="${({detail}: CustomEvent) => {
-                this.assessment!.assessment_type = detail.selectedItem?.value;
+                if (detail.selectedItem) {
+                  this.assessment!.assessment_type = detail.selectedItem.value;
+                }
               }}"
               ?readonly="${this.isReadonly(this.editMode, this.assessment.permissions.edit.assessment_type)}"
               ?required="${this.assessment.permissions.required.assessment_type}"
