0913111be53321cb374f22d2ce0679a66d118913
Trif Adriana <trif.corina.adriana@gmail.com>
2021-06-18 14:23:36 +0300

    fix console err

--- src_ts/components/pages/assessments/assessment-tab-pages/follow-up/follow-up-page.ts
+++ src_ts/components/pages/assessments/assessment-tab-pages/follow-up/follow-up-page.ts
@@ -66,7 +66,7 @@ export class FollowUpPage extends connect(store)(LitElement) {
         <div class="layout-horizontal container">
           <div class="col-4">
             <div class="paper-label">NFR Attachment</div>
-            <div class="input-label" ?empty="${!this.assessment.nfr_attachment}">
+            <div class="input-label" ?empty="${!this.assessment?.nfr_attachment}">
               <a href="${this.assessment?.nfr_attachment}" target="_blank">
                 ${getFileNameFromURL(this.assessment.nfr_attachment)}</a
               >
