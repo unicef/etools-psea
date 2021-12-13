e8bb4a27ac12f2cde1e0d2631492fd4466f7038d
Dan <49279861+danNordlogic@users.noreply.github.com>
2021-12-13 13:07:43 +0200

    [ch27584] Incorrect Date validation for "Due on" (#358)

--- src_ts/components/utils/utils.ts
+++ src_ts/components/utils/utils.ts
@@ -17,3 +17,10 @@ export const getFileNameFromURL = (url?: string) => {
 export const onListPage = (routeDetails: any) => {
   return routeDetails.routeName === 'assessments' && routeDetails.subRouteName == 'list';
 };
+
+export const resetRequiredField = (e: CustomEvent) => {
+  if (!e || !e.target) {
+    return;
+  }
+  (e.target as any).invalid = false;
+};
